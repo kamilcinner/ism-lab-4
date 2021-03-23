bd491396d695465adcff23080a7a8ca701454380
Kamil Cinner <kamil.cinner@gmail.com>
2021-03-23 13:40:45 +0100

    Added some code

--- app/src/main/java/pollub/ism/lab04/MainActivity.java
+++ app/src/main/java/pollub/ism/lab04/MainActivity.java
@@ -23,11 +23,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButtonClick(View view) {
         String name = view.getResources().getResourceEntryName(view.getId());
-        int index = Integer.parseInt(String.valueOf(name.charAt(1))) * 3 + Integer.parseInt(String.valueOf(name.charAt(2)));
 
-        if (this.board[index].equals("")) {
+        int row = Integer.parseInt(String.valueOf(name.charAt(1)));
+        int col = Integer.parseInt(String.valueOf(name.charAt(2)));
+
+        if (this.getBoardFieldValue(row, col).equals("")) {
             ((Button) view).setText(this.currentMove);
-            this.board[index] = this.currentMove;
+            this.setBoardFieldValue(row, col, this.currentMove);
             this.currentMove = this.currentMove.equals("O") ? "X" : "O";
         }
     }
@@ -36,5 +38,80 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 9; i++) {
             this.board[i] = "";
         }
+    }
+
+    private void checkWin() {
+        if (this.isAnyFieldEmpty()) {
+            return;
+        }
+
+        String player = this.checkRows();
+        if (!player.equals("")) {
+            // Player wins
+
+            return;
+        }
+
+        player = this.checkCols();
+        if (!player.equals("")) {
+            // Player wins
+
+            return;
+        }
+    }
+
+    private boolean isAnyFieldEmpty() {
+        for (String field : board) {
+            if (field.equals("")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String checkRows() {
+        String player = "";
+        for (int i = 0; i < 3; i++) {
+            player = this.getBoardFieldValue(i, 0);
+            for (int j = 1; j < 3; j++) {
+                if (!player.equals(this.getBoardFieldValue(i, j))) {
+                    player = "";
+                    break;
+                }
+            }
+            if (!player.equals("")) {
+                break;
+            }
+        }
+        return player;
+    }
+
+    private String checkCols() {
+        String player = "";
+        for (int i = 0; i < 3; i++) {
+            player = this.getBoardFieldValue(0, i);
+            for (int j = 1; j < 3; j++) {
+                if (!player.equals(this.getBoardFieldValue(j, i))) {
+                    player = "";
+                    break;
+                }
+            }
+            if (!player.equals("")) {
+                break;
+            }
+        }
+        return player;
+    }
+
+    private String getBoardFieldValue(int row, int col) {
+        return this.board[this.getBoardIndex(row, col)];
+    }
+
+    private void setBoardFieldValue(int row, int col, String value) {
+        this.board[this.getBoardIndex(row, col)] = value;
+    }
+
+    private int getBoardIndex(int row, int col) {
+        return row * 3 + col;
     }
 }
