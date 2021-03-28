922dcc74487f2430fd110aa8603e9fca11940481
Kamil Cinner <kamil.cinner@gmail.com>
2021-03-28 11:16:07 +0200

    Added some code

--- app/src/main/java/pollub/ism/lab04/MainActivity.java
+++ app/src/main/java/pollub/ism/lab04/MainActivity.java
@@ -5,13 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import java.util.ArrayList;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String currentMove = "O";
+    private String currentMove;
     private final String[] board = new String[9];
+    private boolean gameEnd = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +21,21 @@ public class MainActivity extends AppCompatActivity {
         this.initializeBoard();
     }
 
-    public void onButtonClick(View view) {
-        String name = view.getResources().getResourceEntryName(view.getId());
+    public void onPlayerMove(View view) {
+        if (this.gameEnd) {
+            this.resetBoard();
+        } else {
+            String name = view.getResources().getResourceEntryName(view.getId());
 
-        int row = Integer.parseInt(String.valueOf(name.charAt(1)));
-        int col = Integer.parseInt(String.valueOf(name.charAt(2)));
+            int row = Integer.parseInt(String.valueOf(name.charAt(1)));
+            int col = Integer.parseInt(String.valueOf(name.charAt(2)));
 
-        if (this.getBoardFieldValue(row, col).equals("")) {
-            ((Button) view).setText(this.currentMove);
-            this.setBoardFieldValue(row, col, this.currentMove);
-            this.currentMove = this.currentMove.equals("O") ? "X" : "O";
+            if (this.getBoardFieldValue(row, col).equals("")) {
+                ((Button) view).setText(this.currentMove);
+                this.setBoardFieldValue(row, col, this.currentMove);
+                this.checkWin();
+                this.changePlayer();
+            }
         }
     }
 
@@ -38,27 +43,54 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 9; i++) {
             this.board[i] = "";
         }
+        this.currentMove = "O";
     }
 
     private void checkWin() {
         String player = this.checkRows();
-        if (!player.equals("")) {
-            // Player wins
+        if (player.equals("")) {
+            player = this.checkCols();
+        }
+        if (player.equals("")) {
+            player = this.checkDiagonals();
+        }
 
+        if (!player.equals("")) {
+            // Win
+            this.showWinToast();
+            this.gameEnd = true;
             return;
         }
 
-        player = this.checkCols();
-        if (!player.equals("")) {
-            // Player wins
-
-            return;
-        }
-
-        if (this.isAnyFieldEmpty()) {
+        if (!this.isAnyFieldEmpty()) {
             // Tie
-            return;
+            this.showTieToast();
+            this.gameEnd = true;
         }
+    }
+
+    private void showWinToast() {
+        Toast.makeText(this, this.getWinText(), Toast.LENGTH_LONG).show();
+    }
+
+    private void showTieToast() {
+        Toast.makeText(this, "Tie!", Toast.LENGTH_LONG).show();
+    }
+
+    private void resetBoard() {
+        this.initializeBoard();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String textId = "b" + ((Integer) i).toString() + ((Integer) j).toString();
+                int id = getResources().getIdentifier(textId, "id", getPackageName());
+                ((Button) findViewById(id)).setText("");
+            }
+        }
+        this.gameEnd = false;
+    }
+
+    private void changePlayer() {
+        this.currentMove = this.currentMove.equals("O") ? "X" : "O";
     }
 
     private boolean isAnyFieldEmpty() {
@@ -109,8 +141,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String checkDiagonals() {
-        String player = this.getBoardFieldValue(0, 0);
-
+        String player = this.getBoardFieldValue(1, 1);
+        if (!player.equals("")) {
+            if (player.equals(this.getBoardFieldValue(0, 0))
+                    && player.equals(this.getBoardFieldValue(2, 2))) {
+                return player;
+            }
+            if (player.equals(this.getBoardFieldValue(0, 2))
+                    && player.equals(this.getBoardFieldValue(2, 0))) {
+                return player;
+            }
+        }
+        return "";
     }
 
     private String getBoardFieldValue(int row, int col) {
@@ -123,5 +165,9 @@ public class MainActivity extends AppCompatActivity {
 
     private int getBoardIndex(int row, int col) {
         return row * 3 + col;
+    }
+
+    private String getWinText() {
+        return "Player '" + this.currentMove + "' Wins!";
     }
 }
